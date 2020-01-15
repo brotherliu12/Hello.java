eec087bab1f0f62b6cc5e89c16479f7771e12472
brotherliu12 <41877355+brotherliu12@users.noreply.github.com>
2020-01-15 10:04:12 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+public static void main(String[] args) {
+System.out.println("<Johnny Liu>");
+System.out.println("<Hello.Java>");
+System.out.println("<1-15-2020>");
+System.out.println("<255-003>");
+System.out.println();
+}
+}
+
